270528073522ae097d1de228dba6123e185c02a5
kazuo fujimoto <kazuo.fujimoto2007@gmail.com>
2018-03-26 09:26:20 +0900

    add data _jp and _kr made mosaic_jp as base

--- R/mosaic_jp.R
+++ R/mosaic_jp.R
@@ -16,15 +16,18 @@
 
 library(grid)
 library(RColorBrewer)
+# font setup for vcd/vcdExtra
 my.largs <- list(gp_labels = gpar(fontsize =10, fontfamily = "sans"),
                  gp_varnames = gpar(fontsize = 12, fontfamily = "sans"),
                  gp_text=gpar(fontsize =10, fontfamily = "sans"))
-my.main <- gpar(fontsize = 14,fontfamily = "sans")
-# main title  my.sub <- gpar(fontsize = 10,fontfamily = "sans"")
-# sub title  text_gp <- gpar(fontsize = 12,fontfamily = “sans”)# facet   TR
-# mosaic_jp     L
+my.main <- gpar(fontsize = 14,fontfamily = "sans") # for main title
+my.sub <- gpar(fontsize = 10,fontfamily = "sans") # for sub title
+text_gp <- gpar(fontsize = 12,fontfamily = "sans")# for facet
 
-mosaic_jp0 <- function(.tbl,...){
+#
+# wapper of moisac
+#
+mosaic_jp <- function(.tbl,...){
   mosaic(.tbl,
          labeling_args=my.largs,
          main_gp = my.main,
@@ -32,12 +35,16 @@ mosaic_jp0 <- function(.tbl,...){
          ...)
 }
 
+#
+# colset is the col name of RColorbrewer ex: "Pastel1", "Blues"
+# library(RColorBrewer)
+# display.brewer.all(n=10, exact.n=FALSE) # you can see the color palette
 mosaic_jp1 <- function(.tbl,mtitle="mosaic_jp",colset="Set3"){
   nc <- (dim(.tbl))[2]
   mosaic(.tbl,gp=gpar(fill=brewer.pal(nc,colset),col=0),
-         margins = c(left = 5,top = 4), labeling_args=my.largs,
-         main_gp = my.main,
-         sub_gp = my.sub,
+         margins = c(left = 5,top = 4),
+         labeling_args=my.largs,
+         main_gp = my.main, sub_gp = my.sub,
          rot_labels = c(left = 0, top = 45,right=0),
          keep_aspect_ratio=FALSE,
          main=mtitle,
@@ -46,9 +53,9 @@ mosaic_jp1 <- function(.tbl,mtitle="mosaic_jp",colset="Set3"){
 
 mosaic_jp2 <- function(.tbl,mtitle="mosaic_jp",...){
   mosaic(.tbl,...,
-         margins = c(left = 5,top = 4), labeling_args=my.largs,
-         main_gp = my.main,
-         sub_gp = my.sub,
+         margins = c(left = 5,top = 4),
+         labeling_args=my.largs,
+         main_gp = my.main,sub_gp = my.sub,
          rot_labels = c(left = 0, top = 45,right=0),
          keep_aspect_ratio=FALSE,
          main=mtitle,
