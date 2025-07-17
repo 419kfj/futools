0308f015d861fb81c96a2b301cecb1753a6d99c5
419kfj <kazuo.fujimoto2007@gmail.com>
2025-07-17 20:37:03 +0900

    expand vignitte

--- doc/How_to_use_mosaic.R
+++ doc/How_to_use_mosaic.R
@@ -10,6 +10,9 @@ knitr::opts_chunk$set(
 library(futools)
 library(tidyverse)
 library(vcd)
+library(RColorBrewer)
+library(showtext)
+showtext_auto(TRUE)
 
 ## -----------------------------------------------------------------------------
 data("HairEyeColor") # 髪の色と目の色の対応関係を集計した３元クロス表
@@ -32,4 +35,28 @@ HairEyeColor %>% mosaic(rot_labels = c(left = 0, top = 0, right = 0),)
 ## -----------------------------------------------------------------------------
 HairEyeColor %>% mosaic(Eye ~ Sex + Hair,.,
                         rot_labels = c(left = 0, top = 0, right = 0))
+
+## -----------------------------------------------------------------------------
+# data set
+data(HairEyeColor_jp)
+
+## -----------------------------------------------------------------------------
+mosaic(HairEyeColor,shade = TRUE)
+
+## -----------------------------------------------------------------------------
+# plain mosaic_jp
+mosaic_jp(HairEyeColor_jp)
+
+## -----------------------------------------------------------------------------
+# without cell color
+mosaic_jp2(HairEyeColor_jp)
+
+## -----------------------------------------------------------------------------
+# set cell color by color matrix
+cset <- t(matrix(rep(brewer.pal(4,"Blues"),2),ncol=2))
+mosaic_jp2(HairEyeColor_jp,gp=gpar(fill=cset,col=0))
+
+## -----------------------------------------------------------------------------
+# Pearson residulas
+mosaic_jp2(HairEyeColor_jp,shade=TRUE)
 
